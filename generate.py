fe242992a79749222a12c2d28f8ccf53cbe6f3e2
yeongsang2 <dudtkd1221@gmail.com>
2023-07-18 14:58:22 +0900

    fix generate.py

--- generate.py
+++ generate.py
@@ -8,16 +8,16 @@ def main(
     base_model: str = "beomi/KoAlpaca-Polyglot-12.8B",
     lora_weights: str = "/content/my_alpaca/output/"
     ):
-    config = PeftConfig.from_pretrained("/content/my_alpaca/output/checkpoint-100/")
+
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16
     )
-    model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, quantization_config=bnb_config, device_map={"":0})
-    model = PeftModel.from_pretrained(model, "/content/my_alpaca/output/checkpoint-100/")
-    tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+    model = AutoModelForCausalLM.from_pretrained(base_model, quantization_config=bnb_config, device_map={"":0})
+    model = PeftModel.from_pretrained(model, lora_weights)
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
     
     model.eval()
 
